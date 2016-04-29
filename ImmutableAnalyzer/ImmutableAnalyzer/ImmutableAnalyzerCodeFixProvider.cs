2a52357b6cd75722a9f535e0797542eec0dbaed3
Jan Magerl <cdf@gmx.de>
2016-04-29 23:23:28 +0200

    CodeFix

--- ImmutableAnalyzer/ImmutableAnalyzer/ImmutableAnalyzerCodeFixProvider.cs
+++ ImmutableAnalyzer/ImmutableAnalyzer/ImmutableAnalyzerCodeFixProvider.cs
@@ -2,23 +2,25 @@
 {
     using System.Collections.Immutable;
     using System.Composition;
+    using System.Linq;
     using System.Threading;
     using System.Threading.Tasks;
     using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CodeActions;
     using Microsoft.CodeAnalysis.CodeFixes;
-    using Microsoft.CodeAnalysis.CSharp;
     using Microsoft.CodeAnalysis.CSharp.Syntax;
-    using Microsoft.CodeAnalysis.Rename;
 
     [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(ImmutableAnalyzerCodeFixProvider))]
     [Shared]
     public class ImmutableAnalyzerCodeFixProvider : CodeFixProvider
     {
-        private const string Title = "Make uppercase";
+        private const string TitleProperty = "Remove Seter";
+
+        private const string TitleMethod = "Remove Method";
 
         public sealed override ImmutableArray<string> FixableDiagnosticIds
         {
-            get { return ImmutableArray.Create(ImmutableAnalyzerAnalyzer.DiagnosticIdProperty); }
+            get { return ImmutableArray.Create(ImmutableAnalyzerAnalyzer.DiagnosticIdProperty, ImmutableAnalyzerAnalyzer.DiagnosticIdVoidMethod, ImmutableAnalyzerAnalyzer.DiagnosticIdMethod); }
         }
 
         public sealed override FixAllProvider GetFixAllProvider()
@@ -29,43 +31,36 @@
 
         public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
         {
-            await Task.FromResult(0);
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
 
-            // var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+            var diagnostic = context.Diagnostics.First();
+            if (diagnostic.Id == ImmutableAnalyzerAnalyzer.DiagnosticIdProperty)
+            {
+                var diagnosticSpan = diagnostic.Location.SourceSpan;
+                var declaration = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<AccessorDeclarationSyntax>().First();
+                context.RegisterCodeFix(CodeAction.Create(title: TitleProperty, createChangedDocument: c => RemoveSeter(context.Document, declaration, c), equivalenceKey: TitleProperty), diagnostic);
+            }
+            else
+            {
+                var diagnosticSpan = diagnostic.Location.SourceSpan;
+                var declaration = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<MethodDeclarationSyntax>().First();
 
-            //// TODO: Replace the following code with your own analysis, generating a CodeAction for each fix to suggest
-            // var diagnostic = context.Diagnostics.First();
-            // var diagnosticSpan = diagnostic.Location.SourceSpan;
-
-            //// Find the type declaration identified by the diagnostic.
-            // var declaration = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<TypeDeclarationSyntax>().First();
-
-            //// Register a code action that will invoke the fix.
-            // context.RegisterCodeFix(
-            //    CodeAction.Create(
-            //        title: title,
-            //        createChangedSolution: c => MakeUppercaseAsync(context.Document, declaration, c),
-            //        equivalenceKey: title),
-            //    diagnostic);
+                context.RegisterCodeFix(CodeAction.Create(title: TitleMethod, createChangedDocument: c => RemoveMethod(context.Document, declaration, c), equivalenceKey: TitleMethod), diagnostic);
+            }
         }
 
-        private async Task<Solution> MakeUppercaseAsync(Document document, TypeDeclarationSyntax typeDecl, CancellationToken cancellationToken)
+        private async Task<Document> RemoveSeter(Document document, AccessorDeclarationSyntax accessorDecl, CancellationToken cancellationToken)
         {
-            // Compute new uppercase name.
-            var identifierToken = typeDecl.Identifier;
-            var newName = identifierToken.Text.ToUpperInvariant();
+            var root = await document.GetSyntaxRootAsync(cancellationToken);
+            var newroot = root.RemoveNode(accessorDecl, SyntaxRemoveOptions.KeepNoTrivia);
+            return document.WithSyntaxRoot(newroot);
+        }
 
-            // Get the symbol representing the type to be renamed.
-            var semanticModel = await document.GetSemanticModelAsync(cancellationToken);
-            var typeSymbol = semanticModel.GetDeclaredSymbol(typeDecl, cancellationToken);
-
-            // Produce a new solution that has all references to that type renamed, including the declaration.
-            var originalSolution = document.Project.Solution;
-            var optionSet = originalSolution.Workspace.Options;
-            var newSolution = await Renamer.RenameSymbolAsync(document.Project.Solution, typeSymbol, newName, optionSet, cancellationToken).ConfigureAwait(false);
-
-            // Return the new solution with the now-uppercase type name.
-            return newSolution;
+        private async Task<Document> RemoveMethod(Document document, MethodDeclarationSyntax methodDecl, CancellationToken cancellationToken)
+        {
+            var root = await document.GetSyntaxRootAsync(cancellationToken);
+            var newroot = root.RemoveNode(methodDecl, SyntaxRemoveOptions.KeepNoTrivia);
+            return document.WithSyntaxRoot(newroot);
         }
     }
 }
